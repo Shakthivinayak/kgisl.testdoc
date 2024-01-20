de8445a606ba7cf1ecfffaf4649e098c84d0a0d3
Shakthivinayak <150015236+Shakthivinayak@users.noreply.github.com>
2024-01-20 15:02:49 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,135 @@
+// var a = "CONSOLE PRINT"
+// console.log(a)
+// document.write("DOCUMENT PRINT") 
+// alert("ALERT POP UP WINDOW")
+// document.getElementById("head").innerHTML= "INNERHTML"
+
+// // data types 
+// var a =1
+// var b = "keerthana"
+// var c = null
+// var d =[1,2, "name", true]
+
+// var e ={
+//     firstname :"keerthana",
+//     email:"keerth@gmail.com",
+//     age:34,
+
+//     hobbies:["Reading", "dance"],
+  
+//     info:{
+//         payment :"success",
+//         required: true
+//     }
+
+// }
+
+// console.log(typeof(a))
+// console.log(typeof(b))
+// console.log((d))
+// console.log(e)
+
+let l=20;
+let b=20;
+let s=20;
+let rad=20;
+let p=20;
+let n=20;
+let r=20;
+let a=10;
+
+let x=2
+let y=3
+let w=4
+
+let z=2;
+let time =12;
+let day=3;
+
+document.write("<h1>"+"Area of shapes:"+"</h1>"+"<br>")
+document.write("Area of the square="+(l*b)+"<br>")
+document.write("Area of the triangle="+(s*s*s)+"<br>")
+document.write("Area of the circle="+(3.14*rad*rad)+"<br>")
+document.write("Perimeter of the circle="+(2*3.14*rad)+"<br>")
+document.write("and conditon="+ (10==15&&10==10)+"<br>")
+document.write("or conditon="+ (10==10 || 10==15)+"<br>")
+document.write("compound intrest="+( p*(1+(r/100))^n)+"<br>"+"<hr>")
+document.write("<h1>"+"if else:(control statements)"+"</h1>"+"<br>")
+
+if(a>12)
+{
+    document.write("correct")
+}
+else
+{
+    document.write("wrong"+"<br><hr>")
+}
+
+if(z%2==0)
+{
+document.write("even")
+}
+else{
+    document.write("odd")
+}
+document.write("<br><hr>")
+
+if(time<11)
+{
+document.write("morning")
+}
+else if(time<5){
+ document.write("afternoon")
+}
+else if(time<10){
+    document.write("night")
+}
+else
+{
+    document.write("late night")
+}
+document.write("<br><hr>")
+
+
+
+if(x>y && x>w)
+{
+    document.write("x is bigger")
+}
+else if(y>x && y>w)
+{
+    document.write("y is bigger")
+
+}
+else{
+    document.write("w is bigger")
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
